e8950b82f4dda33d2e0ad70a5789d616c635aea7
unknown <traltt1412@gmail.com>
2021-08-19 17:25:22 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'milka-2022',
@@ -45,6 +47,6 @@ export default {
   },
 
   router: {
-    base: 'milka',
+    base: '/milka/',
   }
 }
